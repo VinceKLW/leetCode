06d5575619fe38e9716414b35ce93fa5a4d1d257
Vincent <vincentklwong@hotmail.com>
2024-10-07 20:43:06 -0400

    medium question solved

--- duplicateInteger.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-         return len(set(nums)) != len(nums)
-    
-# EXPLAINATION: 
-# Create a hashset which is a list that only allows for unique values (no duplicates), then compare size of the two lists.
